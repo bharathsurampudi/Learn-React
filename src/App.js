14e591efb1a1538cb061a7224b3b017a2337562d
bharathsurampudi <bharathsurampudi@gmail.com>
2021-01-31 18:01:46 +1100

    add image using img html tag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import profile from './profile.jpeg'
 
+function Header(props){
+  console.log(props);
+  return(
+    <h1>Learning React with {props.name}</h1>
+  );
+}
+function Main(props){
+  return(
+    <section>
+      <p>Lets build a web app with</p>
+      <img src={"https://github.com/bharathsurampudi.png"} height = {200} alt="man standing with bag on his shoulder"/>
+
+      <ul style = {{textAlign : "left"}}>
+        {props.stacks.map((stack) => (<li key ={stack.id}>{stack.title}</li>))}
+      </ul>
+      </section>
+  );
+}
+function Footer(props){
+  return(
+      <footer>
+        <p>Copyright@{props.year}</p>
+      </footer>
+      
+  );
+}
+const stacks = ["python","SQL","JavaScript","React"];
+const stackObjects = stacks.map((stack,i) => ({id : i, title : stack}));
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Bharath Surampudi"/>
+      <Main stacks = {stackObjects}/>
+      <Footer year = {new Date().getFullYear()}/>
     </div>
   );
 }
