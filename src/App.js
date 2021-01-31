2ec37913b93d59b4138b6fa7aca1651e57a6bda1
bharathsurampudi <bharathsurampudi@gmail.com>
2021-01-31 19:03:08 +1100

    fetching data using hooks from apis using useEffect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-//import logo from './logo.svg';
+import React,{useState,useEffect,useReducer} from "react";
 import './App.css';
-//import profile from './profile.jpeg'
+//https://api.github.com/users/bharathsurampudi
 
-function Header(props){
-  console.log(props);
-  return(
-    <h1>Learning React with {props.name}</h1>
-  );
-}
-function Main(props){
-  return(
-    <section>
-      <p>Lets build a web app with</p>
-      <img src={"https://github.com/bharathsurampudi.png"} height = {200} alt="man standing with bag on his shoulder"/>
+function App({login}) {
+  const [data,setData] = useState(null); 
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then((response) => response.json())
+    .then(setData);
+  },[]);
 
-      <ul style = {{textAlign : "left"}}>
-        {props.stacks.map((stack) => (<li key ={stack.id}>{stack.title}</li>))}
-      </ul>
-      </section>
-  );
-}
-function Footer(props){
-  return(
-      <footer>
-        <p>Copyright@{props.year}</p>
-      </footer>
-      
-  );
-}
-const stacks = ["python","SQL","JavaScript","React"];
-const stackObjects = stacks.map((stack,i) => ({id : i, title : stack}));
-function App() {
+  if(data){
+    return <div>{JSON.stringify(data)}</div>
+  }
+
   return (
-    <div className="App">
-      <Header name="Bharath Surampudi"/>
-      <Main stacks = {stackObjects}/>
-      <Footer year = {new Date().getFullYear()}/>
+    <div>
+      No User Available
     </div>
   );
 }
